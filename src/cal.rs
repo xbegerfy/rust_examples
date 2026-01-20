75130805f203cb1ae9ba0417030b68a4a4b26d41
xbegerfy <xbegerfy@proton.me>
2026-01-20 21:15:11 +0800

    ✨ feat(docs test): 添加函数注释以及基于文档的测试用例

--- src/cal.rs
+++ src/cal.rs
@@ -1,3 +1,12 @@
+/// add two numbers
+/// # Examples
+///
+/// ```
+/// let a = 5;
+/// let b = 6;
+/// let answer = rust_examples::cal::add(a, b);
+/// assert_eq!(11, answer);
+/// ```
 pub fn add(a: i32, b: i32) -> i32 {
     a + b
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,7 @@
 pub mod cal;
 
+pub use cal::add;
+
 pub mod minigrep;
 
 pub mod minigrep_iterator;
